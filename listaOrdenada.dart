efa3ba5300094480e9da4bdd1614361fe6924e88
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-08-19 15:42:05 -0500

    Proyecto agregado

---
+++ listaOrdenada.dart
@@ -0,0 +1,26 @@
+List<int> enteros = [5, 4, 3, 2, 1];
+void main() {
+  listaOrdenada(enteros);
+  Iterable<int> enteros2 = enteros.reversed;
+  print(enteros2);
+}
+
+void listaOrdenada(List<int> lista) {
+  bool ascendente = true;
+  bool descendente = true;
+  for (int i = 0; i <= enteros.length - 2; i++) {
+    if (enteros[i] < enteros[i + 1]) {
+      descendente = false;
+    }
+    if (enteros[i] > enteros[i + 1]) {
+      ascendente = false;
+    }
+  }
+  if (ascendente) {
+    print("La lista está ordenada ascendentemente");
+  } else if (descendente) {
+    print("La lista está ordenada descendentemente");
+  } else {
+    print("La lista no está ordenada");
+  }
+}
