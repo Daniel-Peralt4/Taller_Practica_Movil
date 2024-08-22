75cfa917a726aa3c1a6667c5375a3e43243e6d18
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-08-21 20:41:59 -0500

    correcion variables del punto 3

--- models/Punto-3.dart
+++ models/Punto-3.dart
@@ -3,27 +3,27 @@ import 'dart:io';
 void main() {
   // Solicitar al usuario el número
   stdout.write('Ingrese un número para calcular su raíz cuadrada: ');
-  double? number = double.tryParse(stdin.readLineSync()!);
+  double? numero = double.tryParse(stdin.readLineSync()!);
 
-  if (number == null || number < 0) {
+  if (numero == null || numero < 0) {
     print('Por favor, ingrese un número válido y no negativo.');
     return;
   }
 
   // Llamar a la función para calcular la raíz cuadrada
-  double result = calcularRaizCuadrada(number);
+  double result = calcularRaizCuadrada(numero);
 
   // Mostrar el resultado
-  print('La raíz cuadrada aproximada de $number es: $result');
+  print('La raíz cuadrada aproximada de $numero es: $result');
 }
 
-double calcularRaizCuadrada(double number) {
+double calcularRaizCuadrada(double numero) {
   double epsilon = 0.00001; // Precisión deseada
-  double guess = number / 2.0; // Suposición inicial
+  double aprox = numero / 2.0; // Suposición inicial
 
-  while ((guess * guess - number).abs() > epsilon) {
-    guess = (guess + number / guess) / 2.0;
+  while ((aprox * aprox - numero).abs() > epsilon) {
+    aprox = (aprox + numero / aprox) / 2.0;
   }
 
-  return guess;
+  return aprox;
 }
