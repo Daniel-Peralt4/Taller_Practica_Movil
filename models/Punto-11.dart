a162a75b954387207c4abc076b2b036540e9a47a
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-08-22 08:13:53 -0500

    Punto-11

---
+++ models/Punto-11.dart
@@ -0,0 +1,15 @@
+List<String> palabras = [
+  'agua',
+  'fuego',
+  'agua',
+  'aire',
+  'agua',
+  'fuego',
+  'tierra',
+  'aire'
+];
+void main() {
+  Map<String, int> diccionario = {};
+  int contador = 0;
+  for (var palabra in palabras) {}
+}
