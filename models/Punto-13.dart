a6c2ff19723e5ab49cbbc585b63755e00cc20459
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-08-22 22:00:57 -0500

    Punto-13

---
+++ models/Punto-13.dart
@@ -0,0 +1,38 @@
+Map<String, int> dic1 = {
+  'manzana': 7,
+  'pera': 5,
+  'limon': 2,
+  'papaya': 8,
+  'banano': 10
+};
+Map<String, int> dic2 = {
+  'zanahoria': 12,
+  'plátano': 9,
+  'papaya': 4,
+  'tomate': 6,
+  'manzana': 15,
+  'banano': 3
+};
+Map<String, int> resultado = {};
+void main() {
+  print(dic1);
+  print(dic2);
+  combinacionDics(dic1, dic2);
+  print(resultado);
+}
+
+void combinacionDics(Map<String, int> dic1, Map<String, int> dic2) {
+  for (var clave in dic1.keys) {
+    if (dic2.containsKey(clave)) {
+      resultado[clave] = dic1[clave]! + dic2[clave]!;
+    } else {
+      resultado[clave] = dic1[clave]!;
+    }
+    for (var clave in dic2.keys) {
+      if (!resultado.containsKey(clave)) {
+        resultado[clave] = dic2[clave]!;
+      }
+    }
+  }
+  print(resultado);
+}
