b038faf3d8c4d2983f2418bccf11cfd17ae2a24e
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-08-22 22:33:20 -0500

    Correccion Punto-15

--- models/Punto-15.dart
+++ models/Punto-15.dart
@@ -1,22 +1,7 @@
 List<Map<String, dynamic>> estudiantes = [
-  {
-    'nombre': 'Juan',
-    'primer corte': 3.0,
-    'segundo corte': 4.0,
-    'tercer corte': 2.5
-  },
-  {
-    'nombre': 'Pedro',
-    'primer corte': 3.7,
-    'segundo corte': 4.1,
-    'tercer corte': 3.0
-  },
-  {
-    'nombre': 'Ana',
-    'primer corte': 4.2,
-    'segundo corte': 1.6,
-    'tercer corte': 3.8
-  }
+  {'nombre': 'Juan', 'nota 1': 3.0, 'nota 2': 4.0, 'nota 3': 2.5},
+  {'nombre': 'Pedro', 'nota 1': 3.7, 'nota 2': 4.1, 'nota 3': 3.0},
+  {'nombre': 'Ana', 'nota 1': 4.2, 'nota 2': 1.6, 'nota 3': 3.8}
 ];
 void main() {
   print(estudiantes);
@@ -25,10 +10,9 @@ void main() {
 
 void calcularPromedio(List<Map<String, dynamic>> estudiantes) {
   for (var estudiante in estudiantes) {
-    double promedio = (estudiante['primer corte'] +
-            estudiante['segundo corte'] +
-            estudiante['tercer corte']) /
-        3;
+    double promedio =
+        (estudiante['nota 1'] + estudiante['nota 2'] + estudiante['nota 3']) /
+            3;
     print('El promedio de ${estudiante['nombre']} es $promedio');
   }
 }
