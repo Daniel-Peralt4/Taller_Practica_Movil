3fbb7b179b67cbd85341770da9a8899a812e9166
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-08-22 21:12:07 -0500

    Punto-12

---
+++ models/Punto-12.dart
@@ -0,0 +1,21 @@
+Map<String, double> productos = {
+  "Papas": 100.5,
+  "Cebolla": 98,
+  "Tomate": 99.9,
+  "Pimiento": 100,
+  "Ajo": 120.2,
+  "Zanahoria": 106.4
+};
+void main() {
+  print(productos);
+  descuento(productos);
+  print(productos);
+}
+
+void descuento(Map<String, double> productos) {
+  for (var producto in productos.keys) {
+    if (productos[producto]! > 100) {
+      productos[producto] = productos[producto]! * 0.9;
+    }
+  }
+}
