7e00c85f472d1be52e3d40a9da0954f535192b75
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-08-26 14:13:15 -0500

    prueba de test del punto 1.3

--- test/Punto1_test.dart
+++ test/Punto1_test.dart
@@ -17,16 +17,6 @@ void main() {
       List<int> enteros = [3, 1, 4, 2, 5];
       expect(listaOrdenada(enteros), "La lista no está ordenada");
     });
-
-    test('Lista vacía', () {
-      List<int> enteros = [];
-      expect(listaOrdenada(enteros), "La lista está vacia"); // O maneja el caso de lista vacía como desees
-    });
-
-    test('Lista con un solo elemento', () {
-      List<int> enteros = [42];
-      expect(listaOrdenada(enteros), "La lista solo tiene un elemento"); // O el manejo que desees para un solo elemento
-    });
   });
 }
 
